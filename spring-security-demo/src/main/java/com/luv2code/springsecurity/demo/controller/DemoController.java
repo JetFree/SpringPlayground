d194ee0b6bc3e14a3440d5a34a3ec654f9032d6c
Evgeny <jetfree@yandex.ru>
2020-06-22 13:01:19 +0300

    Added proejct files to do test deploy

---
+++ spring-security-demo/src/main/java/com/luv2code/springsecurity/demo/controller/DemoController.java
@@ -0,0 +1,16 @@
+package com.luv2code.springsecurity.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class DemoController {
+
+	@GetMapping("/")
+	public String showHome() {
+		return "home";
+	}
+}
